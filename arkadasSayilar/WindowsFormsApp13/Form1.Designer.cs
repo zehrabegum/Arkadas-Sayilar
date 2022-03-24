b984303b31b8abd63fdc9f698bb7902bba56fb03
zehrabegum <62847967+zehrabegum@users.noreply.github.com>
2022-03-24 22:03:36 +0300

    Arkadas Sayilar
    Arkadaş sayıları bulma

---
+++ arkadasSayilar/WindowsFormsApp13/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace WindowsFormsApp13
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Y";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(116, 53);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(116, 109);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 175);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 60);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "ARKADAŞ MI";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(180, 175);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 60);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "SON";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ arkadasSayilar/WindowsFormsApp13/Form1.cs
@@ -0,0 +1,121 @@
+﻿/***********************************************************************************************************************************
+ SAKARYA ÜNİVERSİTESİ
+ BİLGİSAYAR VE BİLİŞİM BİLİMLERİ FAKÜLTESİ
+ BİLGİSAYAR MÜHENDİSLİĞİ BÖLÜMÜ
+ NESNEYE DAYALI PROGRAMLAMA DERSİ
+ 2019-2020 BAHAR DÖNEMİ
+ ADI:ZEHRA BEGÜM  SOYADI:AKTOLGA   NUMARASI:B191210062   GRUP:1C GRUBU 1.ÖĞRETİM
+/***********************************************************************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace WindowsFormsApp13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double sayi;
+            double sayi1;
+            double sonuc = 0;
+            double sonuc1 = 0;
+            ListBox listbox = new ListBox();
+            listbox.Location = new Point(300,40);
+            listbox.Size = new Size(100, 200);
+            sayi = Convert.ToDouble(textBox1.Text);
+            //GİRİLEN SAYININ ÇARPANLARINI BULMAYI SAĞLAR VE LİSTBOX'A EKLER.
+            if (sayi > 0)
+            {
+                for (int i = 1; i < sayi; i++)
+                {
+                    if (sayi % i == 0)
+                    {
+                        listbox.Items.Add(i);
+                    }
+                    listbox.Items.Clear();//ARKADAŞ MI BUTONUNA BİR DAHA TIKLAYINCA KONTROLLERİN TEKRAR EKLENMESİ ENGELLENİR. 
+                }
+            }
+            else
+            {
+                MessageBox.Show("POZİTİF BİR SAYI GİRİNİZ:");
+            }
+            Controls.Add(listbox);
+            ListBox listbox1 = new ListBox();
+            listbox1.Location = new Point(420, 40);
+            listbox1.Size = new Size(100, 200);
+            sayi1 = Convert.ToDouble(textBox2.Text);
+            //GİRİLEN SAYININ ÇARPANLARINI BULMAYI SAĞLAR VE LİSTBOX1'E EKLER.
+            if (sayi1 > 0)
+            {
+                for (int i = 1; i < sayi1; i++)
+                {
+                    if (sayi1 % i == 0)
+                    {
+                        listbox1.Items.Add(i);
+                    }
+                    listbox1.Items.Clear();//ARKADAŞ MI BUTONUNA BİR DAHA TIKLAYINCA KONTROLLERİN TEKRAR EKLENMESİ ENGELLENİR.
+                }
+            }
+            else
+            {
+                MessageBox.Show("POZİTİF BİR SAYI GİRİNİZ:");
+            }
+            Controls.Add(listbox1);
+            Label label = new Label();
+            label.Location = new Point(340, 25);
+            label.Text = "X";
+            Controls.Add(label);
+            Label label1 = new Label();
+            label1.Location = new Point(460,25);
+            label1.Text = "Y";
+            Controls.Add(label1);
+            Label label2 = new Label();
+            label2.Location = new Point(200, 250);
+            label2.Text = "TOPLAMLAR=";
+            Controls.Add(label2);
+            TextBox textbox = new TextBox();
+            textbox.Location = new Point(300, 250);
+            textbox.Size = new Size(100, 10);
+            //TEXTBOX'TA SONUÇLARIN TOPLANMIŞ OLMASINI SAĞLAR.
+            for (int i = 1; i < sayi; i++)
+            {
+                if (sayi % i == 0)
+                {
+                    listbox.Items.Add(i);
+                    sonuc += i;
+                }
+                textbox.Text = Convert.ToString(sonuc);
+            }
+            Controls.Add(textbox);
+            TextBox textbox1 = new TextBox();
+            textbox1.Location = new Point(420, 250);
+            textbox1.Size = new Size(100, 10);
+            //TEXTBOX1'DE SONUÇLARIN TOPLANMIŞ OLMASINI SAĞLAR.
+            for (int i = 1; i < sayi1; i++)
+            {
+                if (sayi1 % i == 0)
+                {
+                    listbox1.Items.Add(i);
+                    sonuc1 += i;
+                }
+                textbox1.Text = Convert.ToString(sonuc1);
+            }
+            Controls.Add(textbox1);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
